f965f5a5543ed24340e7f9b6194b51a0a7309942
Shane Lin <ssl2ab@virginia.edu>
2018-10-17 15:41:54 -0400

    Changed delimiter to commas

--- process_jsonl.py
+++ process_jsonl.py
@@ -3,7 +3,8 @@ import json
 import csv
 
 filename = "./tweets91818.json1"
-fields = ["created_at","id","text","in_reply_to_user_id","user__id","user__name","user__screen_name","user__location","user__description","user__followers_count","user__friends_count","user__created_at","user__favourites_count","user__utc_offset","user__time_zone","user__geo_enabled","user__verified","user__statuses_count","user__lang","user__is_translator","user__is_translation_enabled","user__translator_type","geo","coordinates","place","retweet_count","favorite_count","lang"]
+fields = ["created_at", "id", "text", "in_reply_to_user_id", "user__id", "user__name", "user__screen_name", "user__location", "user__description", "user__followers_count", "user__friends_count", "user__created_at", "user__favourites_count", "user__utc_offset",
+          "user__time_zone", "user__geo_enabled", "user__verified", "user__statuses_count", "user__lang", "user__is_translator", "user__is_translation_enabled", "user__translator_type", "geo", "coordinates", "place", "retweet_count", "favorite_count", "lang"]
 
 tweets = []
 f = open(filename)
@@ -17,11 +18,12 @@ for line in f:
         tweet[field] = target
     tweets.append(tweet)
 
-with open(filename+'.csv', 'w') as outfile:  
-    w = csv.DictWriter(outfile, fields, delimiter='|', escapechar=' ', quoting=csv.QUOTE_NONE, lineterminator='\n')
+with open(filename+'.csv', 'w') as outfile:
+    w = csv.DictWriter(outfile, fields, delimiter=',',
+                       escapechar='\\', quoting=csv.QUOTE_ALL, lineterminator='\n')
     w.writeheader()
     for tweet in tweets:
         for key in tweet.keys():
-            tweet[key] = str(tweet[key]).replace("\n", "\\n").replace("\r", "\\n")
+            tweet[key] = str(tweet[key]).replace(
+                "\n", "\\n").replace("\r", "\\n")
         w.writerow(tweet)
-    
