d14144ce0515fd3cb620fe37b9998c4857acd232
Shane Lin <ssl2ab@virginia.edu>
2018-10-17 12:52:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Example code for manipulating Tweet JSONL data from TWARC.
---
+++ process_jsonl.py
@@ -0,0 +1,27 @@
+import io
+import json
+import csv
+
+filename = "./tweets91818.json1"
+fields = ["created_at","id","text","in_reply_to_user_id","user__id","user__name","user__screen_name","user__location","user__description","user__followers_count","user__friends_count","user__created_at","user__favourites_count","user__utc_offset","user__time_zone","user__geo_enabled","user__verified","user__statuses_count","user__lang","user__is_translator","user__is_translation_enabled","user__translator_type","geo","coordinates","place","retweet_count","favorite_count","lang"]
+
+tweets = []
+f = open(filename)
+for line in f:
+    tweet = {}
+    j = json.loads(line)
+    for field in fields:
+        target = j
+        for i in field.split("__"):
+            target = target[i]
+        tweet[field] = target
+    tweets.append(tweet)
+
+with open(filename+'.csv', 'w') as outfile:  
+    w = csv.DictWriter(outfile, fields, delimiter='|', escapechar=' ', quoting=csv.QUOTE_NONE, lineterminator='\n')
+    w.writeheader()
+    for tweet in tweets:
+        for key in tweet.keys():
+            tweet[key] = str(tweet[key]).replace("\n", "\\n").replace("\r", "\\n")
+        w.writerow(tweet)
+    
